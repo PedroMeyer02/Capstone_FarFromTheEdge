90952ef630d08abfba0c41fbe8c3294126455627
PedroMeyer02 <pedromeyer02ca@gmail.com>
2026-02-18 08:28:00 -0500

    A2 Figured out - Dialogue fixed, Wanderer is working

--- Assets/Scripts/CAPSTONE II/UI/SkillAcquiredScreen.cs
+++ Assets/Scripts/CAPSTONE II/UI/SkillAcquiredScreen.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 public class SkillAcquiredScreen : MonoBehaviour
 {
     Animator UIAnim;
+
+    float timer = 0;
+
+    public int skillID = 0;
     
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
         //if(Input.GetKeyDown(KeyCode.X))
-        if(Input.anyKey)
+        if(Input.anyKey && timer >= 2f)
         {
             StartCoroutine(FadeOutAnim());
         }
@@ -25,12 +31,32 @@ public class SkillAcquiredScreen : MonoBehaviour
         UIAnim.SetTrigger("FadeOut");
         yield return new WaitForSeconds(1f);
         
+        if(skillID == 2)
+        {
+            GameManager.Instance.IsPlayerPaused = true;
+            GameManager.Instance.A2FinalDialogueActivate = true;
+            TotemScript script = FindAnyObjectByType<TotemScript>();
+            script.ActivateDialogue();
+        }
+
+        if(skillID == 3)
+        {
+            GameManager.Instance.A4FinalDialogueActivate = true;
+        }
+
         this.gameObject.SetActive(false);
         StopAllCoroutines();
     }
 
     private void OnDisable()
     {
-        GameManager.Instance.IsPlayerPaused = false;
+        if(skillID == 1)
+        {
+            GameManager.Instance.IsPlayerPaused = false;
+        }
+        if (skillID == 2)
+        {
+            GameManager.Instance.IsPlayerPaused = true;
+        }
     }
 }
