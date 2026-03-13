a938a4f4d6cb5d9219cb39a70cfc2dd9c14f6a11
PedroMeyer02 <pedromeyer02ca@gmail.com>
2026-03-13 06:26:54 -0400

    Menus Adjustment

--- Assets/Scripts/Menus/EndSceneMenu.cs
+++ Assets/Scripts/Menus/EndSceneMenu.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,17 +8,22 @@ public class EndSceneMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnApplicationQuit()
     {
-        SceneManager.LoadScene("00_MenuScene");
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif    
     }
+
 }
